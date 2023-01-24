5c0665bc5cc1032e53080eef5d50c7ebbcdb6196
avanim <axm1205@case.edu>
2023-01-23 21:05:27 -0500

    added basic footer with links to github and linkedin

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <a href="https://www.linkedin.com/in/avanimuchhala/" target="_blank">LinkedIn</a>
+            <a href="https://github.com/AvaniMuchhala" target="_blank">GitHub</a>
+        </footer>
+    )
+}
+
+export default Footer;
