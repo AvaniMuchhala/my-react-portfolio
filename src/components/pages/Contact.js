2d35c18bb8abd7dd65ce52893311a79fa9c589cb
avanim <axm1205@case.edu>
2023-01-24 08:54:15 -0500

    added basic contact form and error message if no text and no focus

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// Helper function to check if email is valid
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
 export default function Contact() {
+  // State variables for fields in the form
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // inputType is either name, email, or message
+    const inputType = e.target.id;
+    // inputValue is the text provided for that input field
+    const inputValue = e.target.value;
+
+    // Based on input type, set state of either name, email, or message
+    if (inputType === 'name') {
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  }; 
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    
+    // Check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+      return;
+    }
+
+    alert('Thank you for your submission!');
+
+    // Clear out input after successful form submission
+    setName('');
+    setEmail('');
+    setMessage('');
+    setErrorMessage('');
+  }
+
+  const handleOnBlur = (e) => {
+    console.log('Input lost focus');
+    if (!name && e.target.id === 'name') {
+      setErrorMessage('Name is required.');
+      return;
+    } 
+    if (!email && e.target.id === 'email') {
+      setErrorMessage('Email is required.');
+      return;
+    }
+    if (!message && e.target.id === 'message') {
+      setErrorMessage('Message is required.');
+      return;
+    }
+  } 
+  
+  const handleOnFocus = (e) => {
+    console.log('Input has focus');
+    setErrorMessage('');
+    return;
+  }
+
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1>Contact</h1>
+      <form>
+        <div className="mb-3">
+          <label for="name" className="form-label">Name</label>
+          <input value={name} type="text" className="form-control" id="name" onChange={handleInputChange} onFocus={handleOnFocus} onBlur={handleOnBlur}/>
+        </div>
+
+        <div className="mb-3">
+          <label for="email" className="form-label">Email Address</label>
+          <input value={email} type="email" className="form-control" id="email" onChange={handleInputChange} onFocus={handleOnFocus} onBlur={handleOnBlur}/>
+        </div>
+
+        <div className="mb-3">
+          <label for="message" className="form-label">Message</label>
+          <input value={message} type="text" className="form-control" id="message" onChange={handleInputChange} onFocus={handleOnFocus} onBlur={handleOnBlur}/>
+        </div>
+
+        <button type="submit" className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      
+        <div>
+          <p className="error-text" id='error-message'>{errorMessage}</p>
+        </div>
+      
     </div>
   );
 }
