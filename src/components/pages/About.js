02e7732da0193bcafbe6507c1f2a451f6ab4dc2e
avanim <axm1205@case.edu>
2023-01-23 22:01:02 -0500

    added basic about me section with para and image

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import profilePic from '../../images/profile-pic.jpg';
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
+      <img alt="profile" src={profilePic}></img>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Hi! My name is Avani Muchhala. I graduated from Case Western Reserve University in May 2022 with
+        a B.S.E. in Biomedical Engineering and a minor in Computer Science. Throughout college, I was involved
+        in medical imaging research in which I helped develop machine learning models for medical image
+        analytics used for disease diagnosis and treatment evaluation. Currently, I am enrolled in the Penn LPS
+        Coding Bootcamp to build a stronger foundation in coding while expanding my skill set as I explore the
+        field of web development. Please check out my portfolio and feel free to contact me!
       </p>
     </div>
   );
