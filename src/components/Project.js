53add7b2fd20f96db9e3029ab7e1ce4bf4b618d4
avanim <axm1205@case.edu>
2023-02-10 10:20:55 -0500

    added summaries and list of tools to each project

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,9 +7,13 @@ export default function Project(props) {
         <section className="card col-lg-5 m-3 p-0 border-0 rounded">
             <img src={props.image} className="card-img" alt="project-cover"></img>
             {/* Each card will have hidden box with project title (linked to deployed app) and Github icon (linked to GitHub repo) */}
-            <div className="middle d-flex flex-column align-items-center justify-content-center px-2">
-                <a href={props.deployed} target="_blank" className="links"><h3 className="text"><b>{props.title}</b></h3></a>
-                <a href={props.github} target="_blank" className="links"><i className="fa-brands fa-github github-link mt-0"></i></a>
+            <div className="middle d-flex flex-column align-items-center justify-content-center p-2">
+                <section className="d-flex">
+                    <a href={props.deployed} target="_blank" className="links"><h3 className="text"><b>{props.title}</b></h3></a>
+                    <a href={props.github} target="_blank" className="links"><i className="fa-brands fa-github github-link mt-0"></i></a>
+                </section>
+                <p>{props.summary}</p>
+                <p>Tools: {props.tools}</p>
             </div>
         </section>
     );
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -15,42 +15,54 @@ const projects = [
     title: 'Workout Tracker',
     deployed: 'https://workout-tracker-2023.herokuapp.com/',
     github: 'https://github.com/AvaniMuchhala/workout-tracker',
-    image: workoutTrackerImg
+    image: workoutTrackerImg,
+    summary: 'Web app for users to log and track their workouts and retrieve exercise recommendations to help meet personal fitness goals',
+    tools: 'HTML, CSS/Bootstrap, JavaScript, Node.js, Express.js, MySQL/Sequelize, Handlebars.js'
   },
   {
     id: 2,
     title: 'Word of My Life',
-    deployed: 'https://avanimuchhala.github.io/word-of-the-day-project/',
-    github: 'https://github.com/AvaniMuchhala/word-of-the-day-project',
-    image: wordLifeImg
+    deployed: 'https://avanimuchhala.github.io/word-of-my-life/',
+    github: 'https://github.com/AvaniMuchhala/word-of-my-life',
+    image: wordLifeImg,
+    summary: 'App that generates random word of the day or allows users to search any word, and displays info and related media about that word to make learning new words fun',
+    tools: 'HTML, CSS/Bulma, JavaScript, multiple third-party APIs'
   },
   {
     id: 3,
     title: 'Tech Blog',
     deployed: 'https://avani-tech-blog.herokuapp.com/',
     github: 'https://github.com/AvaniMuchhala/tech-blog',
-    image: techBlogImg
+    image: techBlogImg,
+    summary: 'CMS-style blog site for developers to publish their tech blogs and comment on others\' posts. Place for developers to learn more about technical concepts, stay up-to-date with new technologies, and share knowledge with tech community',
+    tools: 'HTML, CSS/Bootstrap, JavaScript, Node.js, Express.js, MySQL/Sequelize, Handlebars.js'
   },
   {
     id: 4,
     title: 'J.A.T.E (Text Editor)',
     deployed: 'https://warm-lake-93524.herokuapp.com/',
     github: 'https://github.com/AvaniMuchhala/text-editor',
-    image: textEditorImg
+    image: textEditorImg,
+    summary: 'Text editor for creating and storing notes or code snippets regardless of Internet connection. Easily access your saved notes at any time!',
+    tools: 'Node.js, Express.js, Web Manifest, Service Workers, Cache API, IndexedDB'
   },
   {
     id: 5,
     title: 'Weather Dashboard',
     deployed: 'https://avanimuchhala.github.io/weather-dashboard/',
     github: 'https://github.com/AvaniMuchhala/weather-dashboard',
-    image: weatherDashboardImg
+    image: weatherDashboardImg,
+    summary: 'Displays current weather and 5-day forecast for any city that user searches and saves any searched cities. Built so that users can plan a trip based on the weather outlook in their city.',
+    tools: 'HTML, CSS, JavaScript, OpenWeatherMap API, Day.js'
   },
   {
     id: 6,
     title: 'Coding Quiz',
     deployed: 'https://avanimuchhala.github.io/coding-quiz/',
     github: 'https://github.com/AvaniMuchhala/coding-quiz',
-    image: codingQuizImg
+    image: codingQuizImg,
+    summary: 'Timed quiz on JavaScript fundamentals that stores each player\'s scores. Allow coding bootcamp students to test their knowledge and gauge their progress compared to peers.',
+    tools: 'HTML, CSS, JavaScript'
   }
 ];
 
@@ -62,7 +74,7 @@ export default function Portfolio() {
       <section className="row col-10 d-flex flex-row flex-wrap justify-content-center">
         {/* For each object in projects array, render Project component passing in title, links, and image URL in the props */}
         {projects.map(project => (
-          <Project title={project.title} deployed={project.deployed} github={project.github} image={project.image} key={project.id} />
+          <Project title={project.title} deployed={project.deployed} github={project.github} image={project.image} summary={project.summary} tools={project.tools} key={project.id} />
         ))}
       </section>
     </div>
